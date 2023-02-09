1ebc79750d74a1fa864e939e2743e3e08c28544b
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-09 21:48:12 +0100

    Add context

---
+++ src/core/context/app.context.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+import { UsePhonenumberStructure } from "../../features/keyboard/hooks/use.phone";
+
+export const AppContext = createContext({} as UsePhonenumberStructure);
